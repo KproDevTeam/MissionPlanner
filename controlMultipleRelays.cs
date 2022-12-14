60064587c5efb5cbba1973f103ff94ed070d6da3
rnjbdya <ranjai123baidya@gmail.com>
2022-12-14 17:36:21 +0900

    Source for the UI design for multiple relay control

---
+++ controlMultipleRelays.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner
+{
+    public partial class controlMultipleRelays : Form
+    {
+        public controlMultipleRelays()
+        {
+            InitializeComponent();
+        }
+               
+        private void leftOn_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftOff_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftOne_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftTwo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftThree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftFour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void leftFive_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightOn_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightOff_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightOne_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightTwo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightThree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightFour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightFive_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
